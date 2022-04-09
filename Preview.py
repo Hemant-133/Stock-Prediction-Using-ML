d31a780fbc3fe1b7bccb2cd43be698cb14467de5
Hemant-133 <103311337+Hemant-133@users.noreply.github.com>
2022-04-09 16:29:01 +0530

    Update Preview.py

--- Preview.py
+++ Preview.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
-st.header("Amazon Stock Analysis")
+option = st.selectbox(
+     'How would you like to be contacted?',
+     ('Amazon', 'Microsoft', 'Google', 'Apple'))
+
+if option == 'Amazon':
+  st.header("Amazon Stock Analysis")
+else if option == 'Microsoft':
+  st.header("Microsoft Stock Analysis")
+else if option == 'Google':
+  st.header("Google Stock Analysis")
+else if option == 'Apple':
+  st.header("Apple Stock Analysis")
+  
+  
+
+
