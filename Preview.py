8a1a7d583f795ca616180b72a52119218c3eec6f
Hemant-133 <103311337+Hemant-133@users.noreply.github.com>
2022-04-28 22:17:12 +0530

    Update Preview.py

--- Preview.py
+++ Preview.py
@@ -6,14 +6,20 @@ option = st.selectbox(
 
 if option == 'Amazon':
   st.header("Amazon Stock Analysis")
-  st.image('Amazon Stock Analysis\'s - Graph.jpeg', caption='Apple Stock Analysis Graph')
-  st.image('Amazon Stock Analysis\'s - Data.jpeg', caption='Apple Stock Analysis Data')
+  st.image('Amazon Stock Analysis\'s - Graph.jpeg', caption='Amazon Stock Analysis Graph')
+  st.image('Amazon Stock Analysis\'s - Data.jpeg', caption='Amazon Stock Analysis Data')
 elif option == 'Microsoft':
   st.header("Microsoft Stock Analysis")
+  st.image('Microsoft Stock Analysis\'s - Graph.jpeg', caption='Microsoft Stock Analysis Graph')
+  st.image('Microsoft Stock Analysis\'s - Data.jpeg', caption='Microsoft Stock Analysis Data')
 elif option == 'Google':
   st.header("Google Stock Analysis")
+  st.image('Google Stock Analysis\'s - Graph.jpeg', caption='Google Stock Analysis Graph')
+  st.image('Google Stock Analysis\'s - Data.jpeg', caption='Google Stock Analysis Data')
 else :
   st.header("Apple Stock Analysis")
+  st.image('Apple Stock Analysis\'s - Graph.jpeg', caption='Apple Stock Analysis Graph')
+  st.image('Apple Stock Analysis\'s - Data.jpeg', caption='Apple Stock Analysis Data')
   
   
 
